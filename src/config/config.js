37a4ca1c8d65994b55ab33114f833e7d6a8661e1
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-09-02 15:59:57 -0300

    Set MinIO env variables

--- src/config/config.js
+++ src/config/config.js
@@ -6,4 +6,6 @@ module.exports = {
   MINIO_PORT: parseInt(process.env.MINIO_PORT, 10) || 9000,
   MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY || 'secret',
   MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY || 'super-secret',
+  MINIO_BUCKET: process.env.MINIO_BUCKET || 'uploads',
+  MINIO_UPLOAD_FOLDER_NAME: process.env.MINIO_UPLOAD_FOLDER_NAME || 'uploads',
 };
