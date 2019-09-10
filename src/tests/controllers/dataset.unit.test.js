16970ad766c19de1d6529c13bf4af5d2f8acd4b5
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-09-10 16:52:59 -0300

    Add downloadDataset controller test

--- src/tests/controllers/dataset.unit.test.js
+++ src/tests/controllers/dataset.unit.test.js
@@ -1,7 +1,44 @@
 import sinon from 'sinon';
 import httpMocks from 'node-mocks-http';
+import { PassThrough } from 'stream';
 import { Dataset } from '../../controllers';
 import { Dataset as DatasetModel } from '../../models';
+
+describe('Test DownloadDataset controller.', () => {
+  const checkResult = async (expectedStatusCode) => {
+    const res = httpMocks.createResponse();
+    const req = httpMocks.createRequest();
+
+    req.params.datasetName = 'test.csv';
+
+    const result = await Dataset.downloadDataset(req, res);
+    expect(result.statusCode).toBe(expectedStatusCode);
+  };
+
+  const stubDownloadDataset = sinon.stub(DatasetModel, 'downloadDatasetStore');
+
+  it('Testing dataset download succesfully.', () => {
+    const mockedStream = new PassThrough();
+
+    stubDownloadDataset.resolves(mockedStream);
+
+    checkResult(200);
+  });
+
+  it('Testing error on dataset download.', () => {
+    stubDownloadDataset.rejects('S3Error');
+
+    checkResult(500);
+  });
+
+  it('Testing send a invalid filename.', () => {
+    stubDownloadDataset.rejects({
+      message: 'The specified key does not exist.',
+    });
+
+    checkResult(400);
+  });
+});
 
 describe('Test UploadDataset controller.', () => {
   const checkResult = async (req, expectedStatusCode) => {
