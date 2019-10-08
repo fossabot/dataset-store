e3b12c98023ff483787fb5b703b2519fe014172b
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-10-08 16:04:10 -0300

    Fix VALID_DATATYPES env var

--- src/config/config.js
+++ src/config/config.js
@@ -8,8 +8,8 @@ module.exports = {
   MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY || 'super-secret',
   MINIO_BUCKET: process.env.MINIO_BUCKET || 'uploads',
   MINIO_UPLOAD_FOLDER_NAME: process.env.MINIO_UPLOAD_FOLDER_NAME || 'uploads',
-  VALID_DATATYPES: process.env.VALID_DATATYPES || [
-    'Numeric',
+  VALID_DATATYPES: process.env.VALID_DATATYPES.split(',') || [
+    'Numerical',
     'Categorical',
     'Datetime',
   ],
