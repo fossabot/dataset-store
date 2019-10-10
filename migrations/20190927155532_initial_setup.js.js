0a9b4e34b77f9780e4e3744c6a50d1f9014d9b8c
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-10-10 15:02:59 -0300

    Update migrations

--- migrations/20190927155532_initial_setup.js.js
+++ migrations/20190927155532_initial_setup.js.js
@@ -1,50 +1,78 @@
+/* eslint-disable func-names */
+/* eslint-disable consistent-return */
 exports.up = function(knex) {
   return Promise.all([
-    knex.schema.createTable('projects', function(t) {
-      t.string('uuid', 255).primary();
-      t.string('name', 255).notNull();
-      t.dateTime('createdAt').notNull();
+    knex.schema.hasTable('projects').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('projects', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('name', 255).notNull();
+          t.dateTime('createdAt').notNull();
+        });
+      }
+    }),
+    knex.schema.hasTable('experiments').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('experiments', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('name', 255).notNull();
+          t.string('projectId', 255)
+            .references('uuid')
+            .inTable('projects')
+            .notNull();
+          t.string('pipelineIdTrain', 255).nullable();
+          t.string('pipelineIdDeploy', 255).nullable();
+          t.string('datasetId', 255).nullable();
+          t.string('headerId', 255).nullable();
+          t.string('targetColumnId', 255).nullable();
+          t.text('parameters', 'longtext').nullable();
+          t.dateTime('createdAt').notNull();
+          t.integer('position', '3').nullable();
+        });
+      }
     }),
 
-    knex.schema.createTable('experiments', function(t) {
-      t.string('uuid', 255).primary();
-      t.string('name', 255).notNull();
-      t.string('projectId', 255)
-        .references('uuid')
-        .inTable('projects')
-        .notNull();
-      t.string('pipelineId', 255).nullable();
-      t.string('datasetId', 255).nullable();
-      t.string('targetColumnId', 255).nullable();
-      t.text('parameters', 'longtext').nullable();
-      t.dateTime('createdAt').notNull();
+    knex.schema.hasTable('pipelines').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('pipelines', function(t) {
+          t.string('uuid', 255).primary();
+        });
+      }
     }),
 
-    knex.schema.createTable('pipelines', function(t) {
-      t.string('uuid', 255).primary();
+    knex.schema.hasTable('datasets').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('datasets', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('bucketName', 255).notNull();
+          t.string('originalName', 255).notNull();
+        });
+      }
     }),
 
-    knex.schema.createTable('datasets', function(t) {
-      t.string('uuid', 255).primary();
-      t.string('bucketName', 255).notNull();
-      t.string('originalName', 255).notNull();
+    knex.schema.hasTable('headers').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('headers', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('bucketName', 255).notNull();
+          t.string('originalName', 255).notNull();
+        });
+      }
     }),
 
-    knex.schema.createTable('headers', function(t) {
-      t.string('uuid', 255).primary();
-      t.string('bucketName', 255).notNull();
-      t.string('originalName', 255).notNull();
-    }),
-
-    knex.schema.createTable('columns', function(t) {
-      t.string('uuid', 255).primary();
-      t.string('headerId', 255)
-        .references('uuid')
-        .inTable('headers')
-        .notNull();
-      t.string('name', 255).notNull();
-      t.string('datatype', 255).notNull();
-      t.integer('position', 255).notNull();
+    knex.schema.hasTable('columns').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('columns', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('headerId', 255)
+            .references('uuid')
+            .inTable('headers')
+            .notNull();
+          t.string('name', 255).notNull();
+          t.string('datatype', 255).notNull();
+          t.integer('position', 5).notNull();
+        });
+      }
     }),
   ]);
 };
