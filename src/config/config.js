14793ec264b0d98d5a9f67dd33bc4fa860175ee1
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-10-14 14:28:48 -0300

    Update MINIO_HOST default

--- src/config/config.js
+++ src/config/config.js
@@ -2,10 +2,10 @@ import 'dotenv/config';
 
 module.exports = {
   PORT: process.env.HOST_PORT || 4000,
-  MINIO_HOST: process.env.MINIO_HOST || 'minio',
+  MINIO_HOST: process.env.MINIO_HOST || 'minio-service',
   MINIO_PORT: parseInt(process.env.MINIO_PORT, 10) || 9000,
-  MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY || 'secret',
-  MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY || 'super-secret',
+  MINIO_ACCESS_KEY: process.env.MINIO_ACCESS_KEY || 'minio',
+  MINIO_SECRET_KEY: process.env.MINIO_SECRET_KEY || 'minio123',
   MINIO_BUCKET: process.env.MINIO_BUCKET || 'mlpipeline',
   MINIO_UPLOAD_FOLDER_NAME: process.env.MINIO_UPLOAD_FOLDER_NAME || 'uploads',
   VALID_DATATYPES: process.env.VALID_DATATYPES
